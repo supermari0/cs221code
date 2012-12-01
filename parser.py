665e3cf30915a7b3c4eff50b4a1f23e38333afc2
Mario Villaplana <mario.villaplana@gmail.com>
2012-11-30 17:53:59 -0800

    fixed tab issue

--- parser.py
+++ parser.py
@@ -8,36 +8,36 @@ python parser.py <Filename>
 
 def parser(): 
 
-	print("HELLO")
-	filepath = sys.argv[1]
-	wiki_file = open(filepath)
-	lines = wiki_file.readlines() # list of each line in the file as a string
+  print("HELLO")
+  filepath = sys.argv[1]
+  wiki_file = open(filepath)
+  lines = wiki_file.readlines() # list of each line in the file as a string
 
-	for line in lines: 
-		title_obj = re.search('<title>.*</title>' , line)
-		if title_obj != None: 
-			break
+  for line in lines: 
+    title_obj = re.search('<title>.*</title>' , line)
+    if title_obj != None: 
+      break
 
-	title = title_obj.group(0)
-	title = title[7 : (title.find("Wikipedia, the free encyclopedia")-3)]
+  title = title_obj.group(0)
+  title = title[7 : (title.find("Wikipedia, the free encyclopedia")-3)]
 
-	json_file_name = title + '.json'
+  json_file_name = title + '.json'
 
-	json_file = open(json_file_name, 'w') # json file to write to
+  json_file = open(json_file_name, 'w') # json file to write to
 
-	json_file.write('{\n\t')
-	json_file.write('"title" : ' + '"' + title + '",\n')
+  json_file.write('{\n  ')
+  json_file.write('"title" : ' + '"' + title + '",\n')
 
-	links = find_links(lines)
-	write_links_to_json(json_file, links)
-	articleTextIndex = parseSectionHeaders(lines, json_file)
-	#parseArticleText(lines, json_file, articleTextIndex)
+  links = find_links(lines)
+  articleTextIndex = parseSectionHeaders(lines, json_file)
+  write_links_to_json(json_file, links)
+  #parseArticleText(lines, json_file, articleTextIndex)
 
-	json_file.write('}')
+  json_file.write('}')
 
-	json_file.close()
+  json_file.close()
 
-	wiki_file.close()
+  wiki_file.close()
 
 
 """
@@ -49,35 +49,35 @@ Arguments:
 
 def parseArticleText(lines, json_file, article_text_index): 
 
-	entireArticleText = ""
+  entireArticleText = ""
 
-	dab = ""  # disambiguation line
-	for line in lines: 
-		obj = re.search('.*div class="dablink">.*', line)
-		if obj != None: 
-			dab = line
-			break
+  dab = ""  # disambiguation line
+  for line in lines: 
+    obj = re.search('.*div class="dablink">.*', line)
+    if obj != None: 
+      dab = line
+      break
 
-	startIndex = (dab.find("<div class=\"dablink\">") + 21)
-	dab_text = dab[startIndex : len(dab)]
-	endIndex = dab_text.find(">.<")
-	dab_text = dab_text[0 : endIndex+1]
+  startIndex = (dab.find("<div class=\"dablink\">") + 21)
+  dab_text = dab[startIndex : len(dab)]
+  endIndex = dab_text.find(">.<")
+  dab_text = dab_text[0 : endIndex+1]
 
-	entireArticleText += dab_text
+  entireArticleText += dab_text
 
-	end_article_index = 0
+  end_article_index = 0
 
-	for line in lines[article_text_index : len(lines)]: 
-		if "<h2> <span class=\"mw-headline\" id=\"See_also\">See also</span></h2>" in line: 
-			break
-		else: 
-			entireArticleText += line
+  for line in lines[article_text_index : len(lines)]: 
+    if "<h2> <span class=\"mw-headline\" id=\"See_also\">See also</span></h2>" in line: 
+      break
+    else: 
+      entireArticleText += line
 
 
-	
-	json_file.write('\t\t"text" : ' + '"' + entireArticleText + '"' + ',\n')
-	json_file.write('\t\t"length" : ' + str(len(entireArticleText)) + '\n')
-	json_file.write('\t},\n')
+  
+  json_file.write('  "text" : ' + '"' + entireArticleText + '"' + ',\n')
+  json_file.write('  "length" : ' + str(len(entireArticleText)) + '\n')
+  json_file.write('  },\n')
 
 
 """
@@ -90,71 +90,71 @@ Returns the index of lines where the article text starts
 """
 
 def parseSectionHeaders(lines, json_file): 
-	
-	json_file.write('\t"body" : {\n')
+  
+  json_file.write('  "body" : {\n')
 
-	contentsStart = 0
-	for index, line in enumerate(lines): 
-		contents_obj = re.search('<h2>Contents</h2>' , line)
-		if contents_obj != None: 
-			contentsStart = index
-			break
-
-
-	contentsEnd = 0
-	counter = contentsStart
-
-	for line in lines[contentsStart : len(lines)]: 
-		if ('</table>' in line): 
-			contentsEnd = counter
-			break
-		counter += 1
-
-	categories = []
-	
-	for line in lines[contentsStart : contentsEnd]:  # lines[contentsStart : contentsEnd] contains the HTML of the categories table
-		obj = re.search('li class="toclevel-[0-9].*', line)
-		if (obj != None): 
-			#print(line)
-			startIndex = line.find("<span class=\"toctext\">")
-			line = line[(startIndex + 22) : len(line)]
-			endIndex = line.find('<')
-			line = line[0 : endIndex]
-			categories.append(line)
-
-		
-	json_file.write('\t\t"headings" : [\n')
-	json_file.write('\t\t\t"' + categories[0] + '"')
-	for cat in categories[1 : len(categories)]: 
-		json_file.write(',\n\t\t\t"' + cat + '"')
-
-	json_file.write('\n\t\t],\n')
+  contentsStart = 0
+  for index, line in enumerate(lines): 
+    contents_obj = re.search('<h2>Contents</h2>' , line)
+    if contents_obj != None: 
+      contentsStart = index
+      break
 
 
-	return contentsEnd + 1
+  contentsEnd = 0
+  counter = contentsStart
+
+  for line in lines[contentsStart : len(lines)]: 
+    if ('</table>' in line): 
+      contentsEnd = counter
+      break
+    counter += 1
+
+  categories = []
+  
+  for line in lines[contentsStart : contentsEnd]:  # lines[contentsStart : contentsEnd] contains the HTML of the categories table
+    obj = re.search('li class="toclevel-[0-9].*', line)
+    if (obj != None): 
+      #print(line)
+      startIndex = line.find("<span class=\"toctext\">")
+      line = line[(startIndex + 22) : len(line)]
+      endIndex = line.find('<')
+      line = line[0 : endIndex]
+      categories.append(line)
+
+    
+  json_file.write('    "headings" : [\n')
+  json_file.write('      "' + categories[0] + '"')
+  for cat in categories[1 : len(categories)]: 
+    json_file.write(',\n      "' + cat + '"')
+
+  json_file.write('\n    ],\n')
+
+
+  return contentsEnd + 1
 
 def write_links_to_json(json_file, links):
-	json_file.write('"links" : [')
-	for i in range(len(links)):
-		json_file.write('"')
-		json_file.write(links[i])
-		json_file.write('"')
-		if i != len(links) - 1:
-			json_file.write(',')
-	json_file.write(']')
+  json_file.write('"links" : [')
+  for i in range(len(links)):
+    json_file.write('"')
+    json_file.write(links[i])
+    json_file.write('"')
+    if i != len(links) - 1:
+      json_file.write(',')
+  json_file.write(']')
 
 def find_links(lines):
-	links = []
-	for line in lines:
-		link_obj = re.search('<a href="/wiki/.*" title=".*">', line)
-		if link_obj != None:
-			link = link_obj.group(0)
-			link_obj = re.search('title=".*"', link)
-			link = link_obj.group(0)
-			link_obj = re.search('"[^"]*"', link)
-			link = link_obj.group(0)
-			link = link[1:-1]
-			links.append(link)
-	return links
+  links = []
+  for line in lines:
+    link_obj = re.search('<a href="/wiki/.*" title=".*">', line)
+    if link_obj != None:
+      link = link_obj.group(0)
+      link_obj = re.search('title=".*"', link)
+      link = link_obj.group(0)
+      link_obj = re.search('"[^"]*"', link)
+      link = link_obj.group(0)
+      link = link[1:-1]
+      links.append(link)
+  return links
 
 parser()
