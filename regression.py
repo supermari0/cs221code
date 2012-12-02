7dbcf3f91c8cd0d85e32995304da2fd5ee226c14
Ryan Globus <ryanglobus@gmail.com>
2012-12-02 12:41:34 -0800

    Clean up regression.py feature vector functions

--- regression.py
+++ regression.py
@@ -1,23 +1,23 @@
 from article import *
 
-def feature_vector1(article, options):
+def basic_features(article, options):
   # Only uses basic features which can be pulled directly from the JSON data
   return [article.body_length(), article.is_current_event(), article.percent_anonymous_edits()] + \
          [article.percent_top_edits(), article.age()]
 
-def feature_vector2(article, options):
+def token_features(article, options):
   # Takes into account the number of tokens in headings and body text
-  vector = feature_vector1(article, options)
+  vector = []
   heading_tokens = options['heading_tokens']
   body_tokens = options['body_tokens']
   for token in heading_tokens: vector.append(article.heading_tokens().count(token))
   for token in body_tokens: vector.append(article.body_text_tokens().count(token))
   return vector
 
-def feature_vector3(article, options):
+def link_features(article, options):
   # Takes into the total number of edits of articles it links to and the average
   # number of edits than an article links to
-  vector = feature_vector2(article, options)
+  vector = []
   articles = options['articles']
   titles = [a.title() for a in articles]
   links_num_edits = [] # Number of edits of the articles that article links to
@@ -32,7 +32,7 @@ def feature_vector3(article, options):
     vector.append(0)
   return vector
 
-def generate_data(filepaths, feature_vector_fn):
+def generate_data(filepaths):
   articles = [Article(f) for f in filepaths]
   top_editors = []
   heading_tokens = []
@@ -46,7 +46,12 @@ def generate_data(filepaths, feature_vector_fn):
       if token not in body_tokens: body_tokens.append(token)
   options = {'articles': articles, 'top_editors': top_editors, 'heading_tokens': heading_tokens, \
              'body_tokens': body_tokens}
-  return [(feature_vector_fn(a, options), a.num_edits()) for a in articles]
+  data = []
+  for a in articles:
+    feature_vector = basic_features(article, options) + token_features(article, options) + \
+                     link_features(article, options)
+    data.append((feature_vector, a.num_edits()))
+  return data
 
 # Below is just a test
-print generate_data(['Elephant.json', 'Lion.json'], feature_vector3)
+# print generate_data(['Elephant.json', 'Lion.json'])
