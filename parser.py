dca58f1bb8a21bb506a5b656246e2ff5316a786d
rupss <rupsshankar@gmail.com>
2012-12-02 17:38:29 -0800

    Fixed links

--- parser.py
+++ parser.py
@@ -11,12 +11,15 @@ DELIMITER = '%%%'
 def parser():
   directory = sys.argv[1]
   wikiFiles = os.listdir(directory)
+  #json_file_name = 'wiki.json'
+  #json_file = open(json_file_name, 'w')
   for filepath in wikiFiles:
     # Randomly select whether to add this file to training or test set
     if random.random() < 0.5:
       json_file_name = 'train.json'
     else:
       json_file_name = 'test.json'
+    
     json_file = open(json_file_name, 'a') # json file to write to
     if re.search('^index\.html.*', filepath) == None:
       try:
@@ -41,12 +44,13 @@ def parser():
       json_file.write('"title" : ' + '"' + title + '",\n')
 
       links = find_links(lines)
-      articleTextIndex = parseSectionHeaders(lines, json_file)
-      parseArticleText(lines, json_file, articleTextIndex)
-      write_links_to_json(json_file, links)
+      cat_and_index = parse_section_headers(lines, json_file)
+      article_text_index = cat_and_index[1]
+      entire_article_text = parse_article_text(lines, json_file, article_text_index)
+      write_links_to_json(json_file, links, entire_article_text, cat_and_index[0])
 
 
-      parseEdits(edits_lines, json_file)
+      parse_edits(edits_lines, json_file)
 
       most_frequent_users = find_most_frequent_users(edits_lines)
       write_users_to_json(json_file, most_frequent_users)
@@ -54,8 +58,9 @@ def parser():
       json_file.write('}\n' + DELIMITER + '\n')
       wiki_file.close()
       json_file.close()
+  #json_file.close()
 
-def parseEdits(edits_lines, json_file):
+def parse_edits(edits_lines, json_file):
 
   json_file.write('"edits" : {\n')
   edits_line = ""
@@ -68,11 +73,11 @@ def parseEdits(edits_lines, json_file):
       cont = index
       break
 
-  startIndex = edits_line.find('<h2>') + 4
-  endIndex = edits_line.find('edits')
+  start_index = edits_line.find('<h2>') + 4
+  end_index = edits_line.find('edits')
 
-  numEdits = edits_line[startIndex : endIndex]
-  json_file.write('     "total" : ' + numEdits + ',\n')
+  num_edits = edits_line[start_index : end_index]
+  json_file.write('     "total" : ' + num_edits + ',\n')
 
   for line in edits_lines[cont : len(edits_lines)]: 
     obj = re.search('<li>Anonymous user edited [0-9]+ times</li>', line)
@@ -80,10 +85,10 @@ def parseEdits(edits_lines, json_file):
       edits_line = line
       break
 
-  startIndex = edits_line.find('edited')
-  endIndex = edits_line.find('times')
+  start_index = edits_line.find('edited')
+  end_index = edits_line.find('times')
 
-  anon_edits = edits_line[(startIndex + 7) : endIndex]
+  anon_edits = edits_line[(start_index + 7) : end_index]
   json_file.write('    "anonymous" : ' + anon_edits + ',\n')
 
   for line in edits_lines[cont : len(edits_lines)]: 
@@ -92,10 +97,10 @@ def parseEdits(edits_lines, json_file):
       edits_line = line
       break
 
-  startIndex = edits_line.find("users:")
-  edits_line = edits_line[startIndex : len(edits_line)]
-  endIndex = edits_line.find('<')
-  top_10_percent = edits_line[7 : endIndex]
+  start_index = edits_line.find("users:")
+  edits_line = edits_line[start_index : len(edits_line)]
+  end_index = edits_line.find('<')
+  top_10_percent = edits_line[7 : end_index]
 
   json_file.write('    "top_10_percent" : ' + top_10_percent + ',\n')
 
@@ -116,11 +121,13 @@ Arguments:
 1) lines - list of strings from the file
 2) json_file - json_file to write to
 3) article_text_index - index in lines of where the actual article text starts
+
+Returns the entire article text as a string
 """
 
-def parseArticleText(lines, json_file, article_text_index): 
+def parse_article_text(lines, json_file, article_text_index): 
 
-  entireArticleText = ""
+  entire_article_text = ""
 
   dab = ""  # disambiguation line
   for line in lines: 
@@ -129,14 +136,14 @@ def parseArticleText(lines, json_file, article_text_index):
       dab = line
       break
 
-  startIndex = (dab.find("<div class=\"dablink\">") + 21)
-  dab_text = dab[startIndex : len(dab)]
-  endIndex = dab_text.find(">.<")
-  dab_text = dab_text[0 : endIndex+1]
+  start_index = (dab.find("<div class=\"dablink\">") + 21)
+  dab_text = dab[start_index : len(dab)]
+  end_index = dab_text.find(">.<")
+  dab_text = dab_text[0 : end_index+1]
 
-  dab_text.replace("\n", "")
+  dab_text = dab_text.replace("\n", "")
 
-  entireArticleText += dab_text.replace('"', '\\"')
+  entire_article_text += dab_text.replace('"', '\\"')
 
   end_article_index = 0
 
@@ -144,13 +151,15 @@ def parseArticleText(lines, json_file, article_text_index):
     if "<h2> <span class=\"mw-headline\" id=\"See_also\">See also</span></h2>" in line: 
       break
     else: 
-      entireArticleText += line.replace('"', '\\"')
+      entire_article_text += line.replace('"', '\\"')
 
-  entireArticleText.rstrip()
+  entire_article_text = entire_article_text.replace("\n", "")
   
-  #json_file.write('  "text" : ' + '"' + entireArticleText + '"' + ',\n')
-  json_file.write('  "length" : ' + str(len(entireArticleText)) + '\n')
+  json_file.write('  "text" : ' + '"' + entire_article_text + '"' + ',\n')
+  json_file.write('  "length" : ' + str(len(entire_article_text)) + '\n')
   json_file.write('  },\n')
+
+  return entire_article_text
 
 
 """
@@ -158,41 +167,41 @@ Arguments:
 1) lines - list of strings from the file
 2) json_file - json_file to write to
 
-Returns the index of lines where the article text starts
+Returns a tuple: (categories, the index of lines where the article text starts)
 
 """
 
-def parseSectionHeaders(lines, json_file): 
+def parse_section_headers(lines, json_file): 
   
   json_file.write('  "body" : {\n')
 
-  contentsStart = 0
+  contents_start = 0
   for index, line in enumerate(lines): 
     contents_obj = re.search('<h2>Contents</h2>' , line)
     if contents_obj != None: 
-      contentsStart = index
+      contents_start = index
       break
 
 
-  contentsEnd = 0
-  counter = contentsStart
+  contents_end = 0
+  counter = contents_start
 
-  for line in lines[contentsStart : len(lines)]: 
+  for line in lines[contents_start : len(lines)]: 
     if ('</table>' in line): 
-      contentsEnd = counter
+      contents_end = counter
       break
     counter += 1
 
   categories = []
   
-  for line in lines[contentsStart : contentsEnd]:  # lines[contentsStart : contentsEnd] contains the HTML of the categories table
+  for line in lines[contents_start : contents_end]:  # lines[contents_start : contents_end] contains the HTML of the categories table
     obj = re.search('li class="toclevel-[0-9].*', line)
     if (obj != None): 
       #print(line)
-      startIndex = line.find("<span class=\"toctext\">")
-      line = line[(startIndex + 22) : len(line)]
-      endIndex = line.find('<')
-      line = line[0 : endIndex]
+      start_index = line.find("<span class=\"toctext\">")
+      line = line[(start_index + 22) : len(line)]
+      end_index = line.find('<')
+      line = line[0 : end_index]
       categories.append(line)
 
     
@@ -205,31 +214,81 @@ def parseSectionHeaders(lines, json_file):
   json_file.write('\n    ],\n')
 
 
-  return contentsEnd + 1
 
-def write_links_to_json(json_file, links):
-  json_file.write('  "links" : [')
-  for i in range(len(links)):
-    json_file.write('"')
-    json_file.write(links[i])
-    json_file.write('"')
-    if i != len(links) - 1:
-      json_file.write(',\n  ')
-  json_file.write('],\n  ')
+
+  return (categories, contents_end + 1)
+
+def write_links_to_json(json_file, links, entire_article_text, categories):
+
+
+
+  json_file.write('  "links" : [\n')
+  
+  if len(links) > 0: 
+    for i in range(len(links) - 1):
+      number_of_mentions = get_number_of_mentions(links[i], entire_article_text, categories)
+      json_file.write('    { "name" : ' + '"' + links[i] + '" , ' + '"number_of_mentions": ' + str(number_of_mentions) + '},\n')
+
+    number_of_mentions = get_number_of_mentions(links[len(links) - 1], entire_article_text, categories)
+    json_file.write('    { "name" : ' + '"' + links[len(links) - 1] + '" , ' + '"number_of_mentions": ' + str(number_of_mentions) + '}\n')
+  
+
+  """number_of_mentions = get_number_of_mentions('Etymology', entire_article_text, categories)
+  json_file.write('    { "name" : ' + '"' + 'Etymology' + '" , ' + '"number_of_mentions": ' + str(number_of_mentions) + '}\n')
+  """
+  json_file.write('  ],\n')
+
+
+def get_number_of_mentions(link, entire_article_text, categories): 
+  curr = entire_article_text
+  counter = 0
+
+  while True: 
+    index = curr.find(link)
+    if index == -1: 
+      break
+    cmp1 = '\\" title'
+    cmp2 = '\\">'
+
+    if curr[index + len(link) : index + len(link) + 8] != cmp1: 
+ 
+      if curr[index + len(link) : index + len(link) + 3] != cmp2:  
+        counter += 1
+
+    curr = curr[index + len(link): len(curr)]
+
+  return counter
+
 
 def find_links(lines):
   links = []
   for line in lines:
-    link_obj = re.search('<a href="/wiki/.*" title=".*">', line)
+    link_obj = re.search('<a href="/wiki/.*" title=".*">[^<]*</a>', line)
     if link_obj != None:
       link = link_obj.group(0)
-      link_obj = re.search('title=".*"', link)
-      link = link_obj.group(0)
-      link_obj = re.search('"[^"]*"', link)
-      link = link_obj.group(0)
-      link = link[1:-1]
-      links.append(link)
+      rest = link
+      while True: 
+        end_index = rest.find('</a>')
+        start_index = rest.find('<a href')
+        curr_link = rest[start_index : end_index]
+        link_obj = re.search('title=".*"', curr_link)
+        if link_obj != None: 
+          curr_link = link_obj.group(0)
+          link_obj = re.search('"[^"]*"', curr_link)
+          if link_obj != None: 
+            curr_link = link_obj.group(0)
+            curr_link = curr_link[1:-1]
+
+            if curr_link not in links: 
+              links.append(curr_link)
+
+        if end_index == len(rest) - 4: 
+          break
+        rest = rest[end_index + 4 : len(rest)]
+
   return links
+     
+
 
 def find_most_frequent_users(edits_lines):
   ''' Given list of lines in edits file, return a list of tuples containing the
