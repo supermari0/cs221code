e029f8dfb935dc832154d971c1caf997e1b26269
Mario Villaplana <mario.villaplana@gmail.com>
2012-12-02 17:27:50 -0800

    fixed parser bug - quotations were not escaped for table of contents/headings

--- parser.py
+++ parser.py
@@ -11,7 +11,10 @@ DELIMITER = '%%%'
 def parser():
   directory = sys.argv[1]
   wikiFiles = os.listdir(directory)
+  article_count = 0
   for filepath in wikiFiles:
+    if article_count > 500:
+      break
     # Randomly select whether to add this file to training or test set
     if random.random() < 0.5:
       json_file_name = 'train.json'
@@ -55,6 +58,7 @@ def parser():
       json_file.write('}\n' + DELIMITER + '\n')
       wiki_file.close()
       json_file.close()
+      article_count += 1
 
 def parseEdits(edits_lines, json_file):
 
@@ -209,6 +213,7 @@ def parseSectionHeaders(lines, json_file):
   if len(categories) > 0:
     json_file.write('      "' + categories[0] + '"')
     for cat in categories[1 : len(categories)]: 
+      cat = cat.replace('"','\\"')
       json_file.write(',\n      "' + cat + '"')
 
   json_file.write('\n    ],\n')
