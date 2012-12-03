a94c2f208797a795edbf719bf92c29299e49e7ac
Mario Villaplana <mario.villaplana@gmail.com>
2012-12-02 19:54:11 -0800

    Removed prints from regression.py

--- regression.py
+++ regression.py
@@ -68,10 +68,8 @@ def read_articles(filepath):
   articles_appended = 0
   for line in lines:
     if line.strip() == DELIMITER.strip():
-      print(json_string)
       articles.append(Article(json_string))
       articles_appended += 1
-      print('articles appended: ' + str(articles_appended))
       json_string = ""
     else:
       json_string += line
