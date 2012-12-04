27bb0cbadfa2d406577c28e42cdfe2dad4f9b661
Mario Villaplana <mario.villaplana@gmail.com>
2012-12-03 16:11:57 -0800

    OK, we have working data and updated average.py.

--- average.py
+++ average.py
@@ -9,8 +9,19 @@ def get_num_edits(filename):
   articles = read_articles(filename)
   # TODO Get number of edits from each article, average, implement fn to find
   # error on training and test sets.
-  num_edits = [article.num_edits() for article in articles]
-  return num_edits
+  numEdits = [article.num_edits() for article in articles]
+  return numEdits
+
+def run_average_heuristic():
+  numEditsTrain = get_num_edits('train.json') 
+  averageNumEdits = sum(numEditsTrain) / float(len(numEditsTrain))
+  numEditsTest = get_num_edits('test.json')
+
+  differences = []
+  for editCount in numEditsTest:
+    difference = editCount - averageNumEdits
+    difference.append(prediction)
+
 
 if __name__ == '__main__':
   train_edit_counts = get_num_edits('train.json')
