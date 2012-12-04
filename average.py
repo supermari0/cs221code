4e1442b2c081bc07ee8cb3611400ac38d94137b8
Mario Villaplana <mario.villaplana@gmail.com>
2012-12-03 16:06:49 -0800

    Revert to data before link_number_of_mentions was added.

--- average.py
+++ average.py
@@ -3,11 +3,14 @@ from article import *
 from parser import DELIMITER
 from regression import read_articles
 
-def get_num_edits():
+def get_num_edits(filename):
   """ Returns a list whose elements are the number of edits for each article in
   the training set. """
-  articles = read_articles('test.json')
+  articles = read_articles(filename)
   # TODO Get number of edits from each article, average, implement fn to find
   # error on training and test sets.
+  num_edits = [article.num_edits() for article in articles]
+  return num_edits
 
-get_num_edits()
+if __name__ == '__main__':
+  train_edit_counts = get_num_edits('train.json')
