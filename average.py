164baf6347da7299dee20e871f5bad3485a83b5d
Mario Villaplana <mario.villaplana@gmail.com>
2012-12-04 16:05:57 -0800

    added ranking error to average

--- average.py
+++ average.py
@@ -1,7 +1,7 @@
 import json
 from article import *
 from parser import DELIMITER
-from regression import read_articles
+from regression import read_articles,rank,get_pairs
 
 def get_num_edits(filename):
   """ Returns a list whose elements are the number of edits for each article in
@@ -19,6 +19,8 @@ def run_average_heuristic():
 
   differences = []
   percentErrors = []
+  rankTuples = []
+  rankLabel = 0
   for editCount in numEditsTest:
     difference = abs(editCount - averageNumEdits)
     differences.append(difference)
@@ -27,15 +29,30 @@ def run_average_heuristic():
       2)
     percentErrors.append(percentError)
 
+    rankTuples.append((rankLabel, editCount, averageNumEdits))
+    rankLabel += 1
+
   totalDifference = sum(differences)
   averageDifference = sum(differences) / float(len(differences))
   averageError = sum(percentErrors) / len(percentErrors)
+
+  rankedByActualEdits = rank(rankTuples, 1)
+  rankedByPredictedEdits = rank(rankTuples, 2)
+  
+  rankedByActualEditPairs = get_pairs(rankedByActualEdits)
+  rankedByPredictedEditPairs = get_pairs(rankedByPredictedEdits)
+
+  intersectionOfRankPairs = rankedByActualEditPairs.intersection(rankedByPredictedEditPairs)
+  print(len(intersectionOfRankPairs))
+  print(len(rankedByActualEditPairs))
+  rankScore = (float(len(intersectionOfRankPairs)) / len(rankedByActualEditPairs))
 
   print('Sum of differences between actual and predicted values: ' +
     str(totalDifference))
   print('Average difference between actual and predicted values: ' +
     str(averageDifference))
   print('Average error: ' + str(averageError) + '%')
+  print('Ranking score: ' + str(rankScore) + '%')
 
 if __name__ == '__main__':
   run_average_heuristic()
